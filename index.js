6a5d22985b5c24d455c7ead8ef2efe014df511d9
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-11-27 01:55:26 -0300

    try to separate some css variables in other js file

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-let button = document.querySelector("button");
+// import { button_style, li_style } from './index_css'
+let add_person_button = document.querySelector(".add-person-button");
 let list = document.querySelector(".list");
 
 let people = [
-  { name: "John Bender" },
-  { name: "Allison Reynolds" },
-  { name: "Brian Johnson" }
+  { id: 1, name: "John Bender" },
+  { id: 2, name: "Allison Reynolds" },
+  { id: 3, name: "Brian Johnson" }
 ];
 
-let button_style = `border-radius: 10px; background-color: red; color: white;
-                    border: 0px solid white; margin: 0px 5px 0px 0px;`
+let button_style = `border-radius: 15px; background-color: red; color: white;
+                    border: 0px solid white; margin: 0px 5px 0px 0px; width: 25px;
+                    height: 25px;`
 let li_style = `display: flex; flex-direction: row;
                 justify-content: space-between;`
 
+
+let remove_person = (index) => {
+  console.log(index)
+}
+
 const load_list = (people) => {
   list.innerHTML = people.map((item) => {
-    return `<li style="${li_style}">
-              ${item.name} <button style="${button_style}">X</button>
-            </li>`
+    return (
+      `<li style="${li_style}">
+        ${item.name} <button style="${button_style}" onclick="${remove_person(item.id)}">X</button>
+      </li>`
+    )
   }).join("");
 };
-
 load_list(people);
 
-button.onclick = () => {
+add_person_button.onclick = () => {
   let input_name = document.querySelector("#input-name").value;
   let list = document.querySelector(".list").innerHTML;
 
-  people.push({ name: input_name });
-  list += `<li>${input_name}</li>`;
-
-  if (!input_name) return "";
+  people.push({ id: list.length + 1, name: input_name });
+  list += `<li style="${li_style}">
+            ${input_name} <button style="${button_style}">X</button>
+          </li>`;
 
   document.querySelector(".list").innerHTML = list;
+  document.querySelector("#input-name").value = ''
 };
---
+++ index_css.js
@@ -0,0 +1,5 @@
+export let button_style = `border-radius: 15px; background-color: red; color: white;
+                    border: 0px solid white; margin: 0px 5px 0px 0px; width: 25px;
+                    height: 25px;`
+export let li_style = `display: flex; flex-direction: row;
+                justify-content: space-between;`
